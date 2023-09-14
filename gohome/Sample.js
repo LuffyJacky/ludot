b3fdb11c0a5ada4cdcc0f0452c1d0b97aaa247ed
LuffyJacky <luffyjacky@gmail.com>
2023-09-14 21:24:41 +0800

    update

---
+++ gohome/Sample.js
@@ -0,0 +1,77 @@
+$(function () {
+    GetAuthorizationHeader();
+    
+    GetApiResponse();    
+});
+
+function GetAuthorizationHeader() {    
+    const parameter = {
+        grant_type:"client_credentials",
+        client_id: "luffyjacky-9b49b8de-d875-4cec",
+        client_secret: "5ff4eee8-9107-445a-bb05-ccb1a2770b66"
+    };
+    
+    let auth_url = "https://tdx.transportdata.tw/auth/realms/TDXConnect/protocol/openid-connect/token";
+        
+    $.ajax({
+        type: "POST",
+        url: auth_url,
+        crossDomain:true,
+        dataType:'JSON',                
+        data: parameter,
+        async: false,       
+        success: function(data){            
+            $("#accesstoken").text(JSON.stringify(data));                            
+        },
+        error: function (xhr, textStatus, thrownError) {
+            
+        }
+    });          
+}
+
+function GetApiResponse(){    
+    let accesstokenStr = $("#accesstoken").text();    
+
+    let accesstoken = JSON.parse(accesstokenStr);    
+
+
+    var arr1 = []
+    
+    if(accesstoken !=undefined){
+        $.ajax({
+            type: 'GET',
+            url: 'https://tdx.transportdata.tw/api/basic/v2/Bus/EstimatedTimeOfArrival/City/Taichung/35?%24filter=%28RouteUID%20eq%20%27TXG35%27%29%20and%20%28StopUID%20eq%20%27TXG2851%27%29&%24orderby=Direction&%24top=30&%24format=JSON',             
+            headers: {
+                "authorization": "Bearer " + accesstoken.access_token,                
+              },            
+            async: false,
+            success: function (Data) {
+                arr1 = Data;
+                console.log(arr1)
+
+                let ans=document.querySelector("#ans")
+
+                for(let i=0;i<arr1.length;i++){
+                    let arr2=arr1[i];
+                    console.log(arr2.EstimateTime);
+                    $('#apireponse3').text(arr2.EstimateTime/'60'+'分鐘ㄛ');
+                    $('#apireponse4').text(arr2.PlateNumb);
+                }
+
+                $('#apireponse').text(JSON.stringify(Data));
+                $('#apireponse2').text(JSON.stringify(arr1));
+      
+                console.log('Data', Data);
+
+            },
+            error: function (xhr, textStatus, thrownError) {
+                console.log('errorStatus:',textStatus);
+                console.log('Error:',thrownError);
+            }
+        });
+    }
+}
+
+
+
+
