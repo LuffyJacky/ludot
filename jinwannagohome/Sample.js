04724c87bae00a2654239d47b5fa4a9b1e350ccc
LuffyJacky <luffyjacky@gmail.com>
2023-09-14 23:55:48 +0800

    update

--- jinwannagohome/Sample.js
+++ jinwannagohome/Sample.js
@@ -40,7 +40,8 @@ function GetApiResponse(){
     if(accesstoken !=undefined){
         $.ajax({
             type: 'GET',
-            url: 'https://tdx.transportdata.tw/api/basic/v2/Bus/EstimatedTimeOfArrival/City/Taichung/35?%24filter=%28RouteUID%20eq%20%27TXG35%27%29%20and%20%28StopUID%20eq%20%27TXG2851%27%29&%24orderby=Direction&%24top=30&%24format=JSON',             
+             url: 'https://tdx.transportdata.tw/api/basic/v2/Bus/EstimatedTimeOfArrival/City/Taichung/35?%24filter=%28RouteUID%20eq%20%27TXG35%27%29%20and%20%28StopUID%20eq%20%27TXG2851%27%29&%24top=30&%24format=JSON',
+            
             headers: {
                 "authorization": "Bearer " + accesstoken.access_token,                
               },            
@@ -53,9 +54,15 @@ function GetApiResponse(){
 
                 for(let i=0;i<arr1.length;i++){
                     let arr2=arr1[i];
-                    console.log(arr2.EstimateTime);
-                    $('#apireponse3').text(arr2.EstimateTime/'60'+'分鐘ㄛ');
-                    $('#apireponse4').text(arr2.PlateNumb);
+                    console.log(arr2)
+                    if(arr2.StopStatus != 3){
+                        console.log(arr2.EstimateTime);
+                        console.log(arr2.PlateNumb);
+                        $('#apireponse3').text(arr2.EstimateTime / '60'+'分鐘ㄛ');
+                        $('#apireponse4').text(arr2.PlateNumb);
+                    } else{
+                        $('#apireponse3').text("蘇姬下班ㄌ");
+                    }
                 }
 
                 $('#apireponse').text(JSON.stringify(Data));
@@ -70,6 +77,7 @@ function GetApiResponse(){
             }
         });
     }
+
 }
 
 
